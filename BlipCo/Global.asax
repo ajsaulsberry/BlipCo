9e4d5fcc5ae25e0d63d8af2754492367c7157062
ajsaulsberry <ajsaulsberry@outlook.com>
2017-08-10 11:22:37 -0400

    Add project files.

---
+++ BlipCo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BlipCo.MvcApplication" Language="C#" %>
---
+++ BlipCo/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BlipCo.Models
+{
+    public class User
+    {
+        public Guid UserID { get; set; }
+
+        public string Username { get; set; }
+
+        public string TermsStatus { get; set; }
+
+        public DateTime Accepted { get; set; }
+
+        public DateTime Reminder { get; set; }
+    }
+}
